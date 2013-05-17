f4b28042f344567f73f7fafb0454065db1a77d66
Dan Calacci <dan@dcalacci.net>
2013-05-17 13:48:33 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
-#/usr/bin/env bash                                                                                                                            
-                                                                                                                                              
-AWESOME_DIR="$HOME/.config/awesome"                                                                                                           
-REPO_URL="https://github.com/dcalacci/powerarrow-solarized"                                                                                   
-                                                                                                                                              
-if [ -d $AWESOME_DIR ]; then                                                                                                                                    
-    mv $AWESOME_DIR $AWESOME_DIR.bak                                                                                                          
-fi                                                                                                                                            
-                                                                                                                                              
+#/usr/bin/env bash
+
+AWESOME_DIR="$HOME/.config/awesome"
+REPO_URL="https://github.com/dcalacci/powerarrow-solarized"
+
+if [ -d $AWESOME_DIR ]; then
+    mv $AWESOME_DIR $AWESOME_DIR.bak
+fi
+
 git clone $REPO_URL $AWESOME_DIR
 
-sed -i 's/dan/$(id -u -n)/g' $AWESOME_DIR/rc.lua{,.test}
+# replace username
+user=$(id -u -n) 
+sed -i 's/dan/'$user'/g' $AWESOME_DIR/rc.lua{,.test}
